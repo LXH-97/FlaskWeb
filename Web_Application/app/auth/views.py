dcb2d0aa857efc7f0940d8b70d99e6247fb422a4
lxh <904781910@qq.com>
2018-02-08 22:04:13 +0800

    commit

--- Web_Application/app/auth/views.py
+++ Web_Application/app/auth/views.py
@@ -1,9 +1,10 @@
 from . import auth
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, login_required
+from flask_login import login_user, login_required, current_user
 from ..models import User
 from .forms import LoginForm
 from ..email import send_email
+
 
 
 # 登入路由
@@ -47,13 +48,44 @@ def register():
     return render_template('auth/register.html', form=form)
 
 
+# 确认用户账户
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('You have been confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
 
 
+# 在before_app_request处理程序中过滤未确认账户
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated() \
+        and not current_user.confirmed \
+        and request.endpoint[:5] != 'auth':
+        and request.endpoint != 'static':
+       return redirect(url_for('auth.unconfirmed'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_annoymous() or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
 
 
-
-
-
+# 重新发送账户确认邮件
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm Your Account',
+               'auth/email/confirm', user=current_user, token=token)
+    flash("A new confirmation email have been sent to you by email.")
+    return render_template(url_for('main.index'))
 
 
 
